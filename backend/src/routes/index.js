df43f100240c735c724b952c5be2fb418968fc47
GHK73 <gunuruhemanthkumar@gmail.com>
2026-07-01 10:30:45 +0530

    index.js

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,10 +3,12 @@
 import {Router} from "express";
 import healthRoutes from "./health.routes.js";
 import authRoutes from "./auth.routes.js";
+import organizationRoutes from "./organization.routes.js";
 
 const router = Router();
 
 router.use("/health",healthRoutes);
 router.use("/auth",authRoutes);
+router.use("/organization", organizationRoutes);
 
 export default router;
